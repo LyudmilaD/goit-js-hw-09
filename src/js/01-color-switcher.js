46740917cb6e9393333b743e1050758db0c09486
Lyudmila Dermenzhii <lsmolyaninova@gmail.com>
2022-02-16 15:29:08 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const btnStartEl = document.querySelector('button[data-start]');
+const btnStopEl = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
+const btnsEl = document.querySelectorAll('button');
 
+let timerId;
+
+btnStartEl.addEventListener('click', onBtnStartClick);
+btnStopEl.addEventListener('click', onBtnStopClick);
+
+function onBtnStartClick(e) {
+  btnStartEl.disabled = true;
+  btnStopEl.disabled = false;
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+btnStartEl.removeEventListener('click', onBtnStopClick);
+function onBtnStopClick(e) {
+  clearTimeout(timerId);
+  btnStopEl.disabled = true;
+  btnStartEl.disabled = false;
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}

3bfdd9bbe06e1a1e562f18a0a8caae622a8aa661
Lyudmila Dermenzhii <lsmolyaninova@gmail.com>
2022-02-18 20:55:48 +0200

    fix promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,5 +1,31 @@
 
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// Напиши скрипт, который при сабмите формы вызывает функцию createPromise(position, delay) столько раз, 
+//сколько ввели в поле amount.При каждом вызове передай ей номер создаваемого промиса(position) и задержку 
+//учитывая введенную пользователем первую задержку(delay) и шаг(step).
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
+// Дополни код функции createPromise так, чтобы она возвращала один промис, который выполянется или отклоняется
+//через delay времени.Значением промиса должен быть объект, в котором будут свойства position и delay 
+//со значениями одноименных параметров.Используй начальный код функции для выбора того, что нужно сделать 
+//с промисом - выполнить или отклонить.
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
+
+
+import Notiflix from 'notiflix';
 
 const form = document.querySelector('.form');
 const delayEl = document.querySelector('input[name="delay"]');
@@ -8,13 +34,14 @@ const amountEl = document.querySelector('input[name="amount"]');
 
 form.addEventListener('submit', onFormSubmit);
 
-function onFormSubmit(e) {
-  e.preventDefault();
+function onFormSubmit(event) {
+  event.preventDefault();
   const firstDelay = Number(`${delayEl.value}`);
   const stepDelay = Number(`${stepEl.value}`);
-  console.log(stepDelay);
   const promiseCount = Number(`${amountEl.value}`);
-
+  
+  console.log(firstDelay);
+  console.log(stepDelay);
   console.log(promiseCount);
 
   let currentDelay = firstDelay;
@@ -24,17 +51,31 @@ function onFormSubmit(e) {
     }
 
     createPromise(i + 1, currentDelay);
-  
-  }
-
-  function createPromise(position, delay) {
-    const shouldResolve = Math.random() > 0.3;
-    setTimeout(() => {
-      if (shouldResolve) {
-        return Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      } else {
-        return Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-      }
-    }, delay);
   }
 }
+
+function createPromise(position, delay) {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const shouldResolve = Math.random() > 0.3;
+
+        if (shouldResolve) {
+          resolve({ position, delay });
+        }
+        
+        else {
+          reject({ position, delay });
+        }
+
+      }, delay);
+    });
+
+  promise
+    .then(({ position, delay }) => {
+      Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+    });
+}
+
